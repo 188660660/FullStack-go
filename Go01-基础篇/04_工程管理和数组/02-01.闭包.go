f2d44db85496d77f6b72b42e8863b44e395c6772
niequn <188660660@server.com>
2019-06-09 11:26:44 +0800

    更新闭包例题

---
+++ Go01-基础篇/04_工程管理和数组/02-01.闭包.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func test1(a int) {
+	a++
+	fmt.Println(a)
+}
+
+func main0201() {
+	/*
+		!总结： 什么是闭包？
+	*/
+	a := 0
+	for i := 0; i <= 5; i++ {
+		//函数会在调用结束后从内存中销毁
+		test1(a)
+	}
+	fmt.Println(a)
+}
+
+func test2() func() int {
+	//可以通过匿名函数和闭包 实现函数在栈区的本地化
+	var a int
+	return func() int {
+		a++
+		return a
+	}
+}
+
+func main0202() {
+	////将test2函数类型赋值给f
+	//f := test2
+	f := test2() //函数调用 将test2的返回值赋值给test2
+	for i := 0; i < 5; i++ {
+		fmt.Println(f())
+	}
+	fmt.Printf("%T", f) //func() int
+}
---
+++ Go01-基础篇/04_工程管理和数组/02-02.闭包.go
@@ -0,0 +1,113 @@
+package main
+
+import "fmt"
+
+func main020201() {
+	/*
+		1.闭包是函数中的匿名函数
+		2.形成闭包的条件必须内部函数要返回出来
+		3.每个闭包都独立保存自己的信息
+	*/
+	fun := fun1() //函数的调用
+	fun()
+}
+
+func fun1() func() {
+	var add string = "上海"
+	fun2 := func() { //匿名函数就是闭包
+		fmt.Println(add)
+	}
+	return fun2 //闭包形成的条件将匿名函数返回出来
+}
+
+//再次练习
+func main020202() {
+	f := fun2()
+	f()
+}
+
+func fun2() func() {
+	a := "中国"
+	fun := func() {
+		fmt.Println(a)
+	}
+	return fun
+}
+
+func main020203() {
+	fmt.Println(fun3()) //1
+	fmt.Println(fun3()) //1
+	fmt.Println(fun3()) //1
+}
+
+func fun3() int {
+	x := 0
+	x++
+	//函数执行完毕后 局部变量即销毁 所以此处每次的存储空间都是不同的
+	fmt.Printf("%p\n", &x) //表示为十六进制，并加上前导的0x &符为取址
+	return x * x
+}
+
+func main020204() {
+	fmt.Println(fun4()) //1
+	fmt.Println(fun4()) //1
+	fmt.Println(fun4()) //1
+
+	fun := fun4
+	fmt.Println(fun())
+	fmt.Println(fun())
+	fmt.Println(fun())
+}
+
+func fun4() func() int {
+	//验证和视频出的结果是否一致：
+
+	//return func() int {
+	//	x := 0
+	//	x++
+	//	//fmt.Printf("%p\n", &x)
+	//	return x * x
+	//}
+
+	var x int = 0
+	return func() int {
+		x++
+		return x * x
+	}
+
+	//闭包每次指向的内存空间都是一样的
+}
+
+func main020205() {
+	var fn [3]func() //函数数组
+	for i := 0; i < 3; i++ {
+		fn[i] = func() {
+			fmt.Println(i)
+		}
+	}
+	//每个调用都是打印3 原因是i变量只有一个 被三个函数共同访问
+	fn[0]() //3
+	fn[1]() //3
+	fn[2]() //3
+
+	for i := 0; i < 3; i++ {
+		fn[i] = make_fun(i)
+	}
+
+	fn[0]() //0
+	fn[1]() //1
+	fn[2]() //2
+	//闭包可以保存自己独有的信息
+	//此处的运行结果不是很理解
+}
+
+func make_fun(i int) func() {
+	return func() {
+		fmt.Println(i)
+	}
+	/*
+		！总结：
+			闭包就像气球 没调用一次就产生一个气球
+			每个气球中的气体可以不一样
+	*/
+}
