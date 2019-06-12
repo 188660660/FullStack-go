61a0e137dcee5c4d3f1ef2e45d4e9fbd9abfd17c
“188660660” <20714428+188660660@users.noreply.github.com>
2019-06-13 01:21:54 +0800

    更新结构体例题
    Signed-off-by: “188660660” <20714428+188660660@users.noreply.github.com>

--- Go01-基础篇/06_结构和指针/06.结构体作为函数参数.go
+++ Go01-基础篇/06_结构和指针/06.结构体作为函数参数.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	id    int
+	name  string
+	score int
+	sex   string
+}
+
+func test060601(s person) {
+	s.name = "刘备"
+	fmt.Println(s)             //{101 刘备 9 男}
+	fmt.Printf("函数内:%T\n", s)  //main.person
+	fmt.Printf("函数内:%p\n", &s) //0xc00007a330
+}
+
+func main060601() {
+	stu := person{101, "宋江", 9, "男"}
+
+	/*
+		结构体作为函数参数 值传递
+		函数内部改变不会影响外部的值
+	*/
+	test060601(stu)
+
+	fmt.Println(stu)             //{101 宋江 9 男}
+	fmt.Printf("函数外:%T\n", stu)  //main.person
+	fmt.Printf("函数外:%p\n", &stu) //0xc00007a300
+}
+
+//所有的切片都属于地址传递
+func test060602(stu []person) {
+	stu[0].name = "晁盖"
+}
+
+func main060602() {
+	//结构体切片
+	stus := []person{
+		{101, "宋江", 9, "男"},
+		{101, "宋江", 9, "男"},
+		{101, "宋江", 9, "男"},
+	}
+
+	//为切片添加信息
+	stus = append(stus, person{102, "张飞", 10, "男"})
+	fmt.Println(stus) //[{101 宋江 9 男} {101 宋江 9 男} {101 宋江 9 男} {102 张飞 10 男}]
+	//结构体切片作为函数参数是地址传递
+	test060602(stus)
+	//结构体数组作为函数参数是值传递
+	fmt.Println(stus) //[{101 晁盖 9 男} {101 宋江 9 男} {101 宋江 9 男} {102 张飞 10 男}]
 }
--- Go01-基础篇/06_结构和指针/08.结构体练习.go
+++ Go01-基础篇/06_结构和指针/08.结构体练习.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+//定义结构体存储5名学生 三门成绩 求出每名学生的总成绩和平均成绩
+type Student060801 struct {
+	id    int
+	name  string
+	score []int //成绩存储为切片类型
+}
+
+func main060801() {
+	//用结构体定义学生的信息
+	stuinfo := []Student060801{
+		{1, "小明", []int{85, 62, 78}},
+		{2, "小王", []int{85, 63, 78}},
+		{3, "小李", []int{85, 64, 78}},
+		{4, "小张", []int{85, 65, 78}},
+		{5, "小红", []int{85, 66, 78}},
+	}
+
+	//五名学生
+	for i := 0; i < len(stuinfo); i++ {
+		sum := 0
+		var pvg float64
+		//三门成绩
+		for j := 0; j < len(stuinfo[i].score); j++ {
+			sum += stuinfo[i].score[j]
+		}
+		pvg = float64(sum) / 3
+		fmt.Printf("%s的总成绩为：%d 平均成绩为：%.2f\n", stuinfo[i].name, sum, pvg)
+	}
 }
