d0d9d3adcc82c6661587ad49afd5844f88f481c2
“188660660” <20714428+188660660@users.noreply.github.com>
2019-06-13 15:06:34 +0800

    更新结构指针例题
    Signed-off-by: “188660660” <20714428+188660660@users.noreply.github.com>

--- Go01-基础篇/06_结构和指针/10.指针变量new操作.go
+++ Go01-基础篇/06_结构和指针/10.指针变量new操作.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main061001() {
+	var p *int
+	fmt.Println(p) //nil
+
+	//为指针变量创建一块内存空间 - 堆区
+	p = new(int)
+	//打印P存储空间的值
+	fmt.Println(p) //0xc00000a0f0 创建好的空间值为数据类型的默认值
+	//打印P指向空间的值
+	fmt.Println(*p)
+
+	var a int
+	fmt.Println(&a) //0xc00000a0f8 8为8个bit 字节 结论验证成立
 }
--- Go01-基础篇/06_结构和指针/11.指针作为函数参数.go
+++ Go01-基础篇/06_结构和指针/11.指针作为函数参数.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func swap(a *int, b *int) {
+	*a, *b = *b, *a
+}
+
+//函数参数为指针类型
+func main061101() {
+	a := 10
+	b := 20
+
+	//值未作交换 值传递
+	//swap(a, b) //10,20
+	//指针作为函数参数传递是地址传递
+	swap(&a, &b)
+	fmt.Println(a) //20
+	fmt.Println(b) //10
 }
