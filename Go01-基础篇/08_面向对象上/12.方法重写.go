c2927e3cafb8c4ca489a0922525027737e253a9c
“188660660” <20714428+188660660@users.noreply.github.com>
2019-06-15 01:54:47 +0800

    更新面向对象例题
    Signed-off-by: “188660660” <20714428+188660660@users.noreply.github.com>

--- Go01-基础篇/08_面向对象上/12.方法重写.go
+++ Go01-基础篇/08_面向对象上/12.方法重写.go
@@ -1,5 +1,38 @@
 package main
 
-func main12() {
+import "fmt"
 
+type person081201 struct {
+	name string
+	age  int
+	sex  string
+}
+
+type student081201 struct {
+	person081201
+	score int
+}
+
+func (s person081201) PrintInfo081201() {
+	fmt.Printf("姓名：%s | 年龄：%d | 性别：%s | 成绩：%d\n", s.name, s.age, s.sex)
+}
+
+/*
+	方法重写 在一个对象中不能出现相同的方法名
+	方法的接收者 带* 和不带* 表示一个相同的对象(本文指 student081201)
+*/
+func (s student081201) PrintInfo081201() {
+	fmt.Printf("姓名：%s | 年龄：%d | 性别：%s | 成绩：%d\n", s.name, s.age, s.sex, s.score)
+}
+
+//方法和函数可以重名 但方法不可以同时出现两个重名的
+func main081201() {
+	t := student081201{person081201{"神王", 999, "未知"}, 888}
+	//默认使用子类的方法 采用就近原则
+	//调用子类的方法
+	t.PrintInfo081201()                   //姓名：神王 | 年龄：999 | 性别：未知 | 成绩：888
+	fmt.Printf("%p\n", t.PrintInfo081201) //0x493230
+	//调用父类的方法
+	t.person081201.PrintInfo081201()                   //姓名：神王 | 年龄：999 | 性别：未知 | 成绩：%!d(MISSING)
+	fmt.Printf("%p\n", t.person081201.PrintInfo081201) //0x4932b0
 }
--- Go01-基础篇/08_面向对象上/13.方法类型和方法值.go
+++ Go01-基础篇/08_面向对象上/13.方法类型和方法值.go
@@ -1,5 +1,58 @@
 package main
 
-func main13() {
+import "fmt"
 
+type person081301 struct {
+	name string
+	age  int
+	sex  string
+}
+
+type student081301 struct {
+	person081301
+	score int
+}
+
+func (p *person081301) PrintInfo081301() {
+	fmt.Printf("大家好，我是%s，我今年%d岁，我是%s生\n", p.name, p.age, p.sex)
+}
+
+func (s *student081301) PrintInfo081301(name string) {
+	s.name = name
+	fmt.Printf("大家好，我是%s，我今年%d岁，我是%s生,我的分数是%d分\n", s.name, s.age, s.sex, s.score)
+}
+
+func hello() {
+	fmt.Println("hello")
+}
+
+type funcdemo func()
+
+func main081301() {
+	s := student081301{person081301{"张三", 11, "男"}, 19}
+
+	//存储在代码区
+	fmt.Println(s.PrintInfo081301)              //0x4932c0
+	fmt.Println(s.person081301.PrintInfo081301) //0x4932c0
+
+	fmt.Println(hello) //0x492e20
+
+	var f1 funcdemo
+	//f1 = s.PrintInfo081301
+	//fmt.Println(f1) //0x493470
+	fmt.Printf("%T\n", f1) //main.funcdemo
+
+	f := hello
+	fmt.Printf("%T\n", f) //func()
+	f()                   //hello
+
+	a := 10
+	fmt.Printf("%p\n", &a) //0xc000062088
+
+	//方法类型(函数类型) 变量
+	f3 := s.PrintInfo081301
+
+	f3("神王")
+	fmt.Println(s)
+	fmt.Printf("%T\n", f3) //func(string)
 }
