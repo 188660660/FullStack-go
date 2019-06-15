3ddc6496e83877eb8765570a363c08c5e404b787
“188660660” <20714428+188660660@users.noreply.github.com>
2019-06-15 23:56:48 +0800

    更新面向对象例题
    Signed-off-by: “188660660” <20714428+188660660@users.noreply.github.com>

--- Go01-基础篇/09_面向对象下/06.空接口定义和使用.go
+++ Go01-基础篇/09_面向对象下/06.空接口定义和使用.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import "fmt"
 
+/*
+	空接口的定义和特性：
+		接口中不包含任何方法,所有的类型都实现了空接口
+		空接口可以存储任意类型的数据
+*/
+func main090601() {
+	var Tnil1 interface{} //空接口 可以保存任意类型数据
+
+	Tnil1 = 10                      //整形
+	Tnil1 = "abc"                   //字符串型
+	Tnil1 = 3.14                    //浮点型
+	Tnil1 = [3]string{"A", "B"}     //数组
+	Tnil1 = []string{"A", "B", "C"} //切片
+
+	fmt.Println(Tnil1)
+}
+
+func main090602() {
+	//fmt.Println("hello")
+	//fmt.Println(10)
+	//fmt.Println(3.14)
+	//fmt.Println(test)
+
+	//空接口 可以接受任意类型数据
+	var i interface{}
+	i = 10
+	fmt.Println(i)         //10
+	fmt.Printf("%p\n", &i) //0xc0000501c0
+
+	i = "hello world"
+	fmt.Println(i)         //hello world
+	fmt.Printf("%p\n", &i) //0xc0000441d0
+
+	var arr [3]int = [3]int{1, 2, 3}
+	i = arr
+	fmt.Println(i)         //[1 2 3]
+	fmt.Printf("%p\n", &i) //0xc0000501c0
+}
+
+func main090603() {
+	//空接口切片
+	var i []interface{}
+	i = append(i, 1, 2, "hello", "你瞅啥", [3]int{1, 2, 3})
+
+	for j := 0; j < len(i); j++ {
+		fmt.Println(i[j])
+	}
+
+	for k, v := range i {
+		fmt.Println(k, v)
+	}
 }
--- Go01-基础篇/09_面向对象下/07.类型断言.go
+++ Go01-基础篇/09_面向对象下/07.类型断言.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import "fmt"
 
+/*
+	value, ok := x.(T)
+	x表示要断言的接口变量；
+	T表示要断言的目标类型；
+	value表示断言成功之后目标类型变量；
+	ok表示断言的结果，是一个bool型变量，true表示断言成功，false表示失败，如果失败value的值为nil。
+
+	网上百度 golang中x.(type)只能在switch中使用
+*/
+func main090701() {
+	arr := make([]interface{}, 6)
+	arr[0] = 1
+	arr[1] = 3.14
+	arr[2] = "字符串"
+	arr[3] = 'a'
+	arr[4] = true
+	arr[5] = []int{1, 2, 3}
+
+	//方法一 for 循环实现
+	for _, v := range arr {
+		//data,ok := v.([]int)
+		//if ok {
+		//	fmt.Println(data)
+		//}
+		if data, ok := v.(int); ok {
+			fmt.Println(data, "是整形")
+		} else if data, ok := v.(float64); ok {
+			fmt.Println(data, "是浮点型")
+		} else if data, ok := v.(byte); ok {
+			fmt.Println(data, "是字符型")
+		} else if data, ok := v.(string); ok {
+			fmt.Println(data, "是字符串型")
+		} else if data, ok := v.(bool); ok {
+			fmt.Println(data, "是布尔型")
+		} else if data, ok := v.([]int); ok {
+			fmt.Println(data, "是整形切片")
+		} else {
+			fmt.Println(data, "未知类型")
+		}
+	}
+
+	//方法二 使用switch 实现
+	for _, v := range arr {
+		switch value := v.(type) {
+		case int:
+			fmt.Println(value, "是整形")
+		case float64:
+			fmt.Println(value, "是字符串型")
+		case []int:
+			fmt.Println(value, "是整形切片")
+		default:
+			fmt.Println("未知类型")
+		}
+	}
 }
